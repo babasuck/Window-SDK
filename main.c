fa3f6eb3c6c8e0e317f744b4dba5ab073e37c787
babasuck <bidanocka@gmail.com>
2024-03-31 00:31:54 +0800

    beta init

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 int WinMain() {
 	struct Window* win = window_create_object();
 	window_ctor(win, 0, L"Test", 500, 500);
+	// beta init
 	struct Line* line = line_ctor(win, 100, 100);
 	window_start_rendering(win);
 	window_start_message_handling(win);
